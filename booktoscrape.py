cf4b7437b4cb02aedaf16e0ea3b5770f4a2e6245
Maxence0105 <79105150+Maxence0105@users.noreply.github.com>
2021-04-06 19:02:11 +0200

    Add files via upload

--- booktoscrape.py
+++ booktoscrape.py
@@ -2,27 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-
-#Pour récupérer l'ensemble des catégorie (problème index de la liste car str et non int/slice)
-
 url = 'http://books.toscrape.com/index.html'
-ctgLinks = []
-bookLinks = []
-bookData = {}
 
+#Pour récupérer l'ensemble des catégories
 def get_categories(url):
+	ctgLinks = []
 	response = requests.get(url)
 	if response.ok:
 		soup = BeautifulSoup(response.text, 'html.parser')
 		menu = soup.find('ul', {'class': 'nav nav-list'})
 		categories = menu.findAll('a')
 		for categorie in categories:
-			ctg = 'http://books.toscrape.com/' + categorie['href']
+			ctg = 'http://books.toscrape.com/' + categorie['href'].replace('index.html','')
 			ctgLinks.append(ctg)
 	return ctgLinks[1:]
 
-#Pour récupérer l'ensemble des livres d'une catégorie (terminé)
-def get_book_urls(ctglinks):
+#Pour récupérer l'ensemble des livres d'une catégorie
+def get_book_urls(url):
+	bookLinks = []
 	response = requests.get(url)
 	if response.ok:
 		soup = BeautifulSoup(response.text, 'lxml')
@@ -31,17 +28,18 @@ def get_book_urls(ctglinks):
 			a = article.find('a')['href'].replace('../../..','http://books.toscrape.com/catalogue')
 			bookLinks.append(a)
 			next = soup.find('li', {'class': 'next'})	
-			if next:
-				link = next.find('a')['href']
-				suite = url.replace('index.html',link)
-				bookLinks.append(get_book_urls(suite))
+		if next:
+			link = next.find('a')['href']
+			suite = suite = url + link
+			bookLinks += get_book_urls(suite)
 	return bookLinks
 
-#Pour récuperer toutes les informations d'un livre (terminé)
-def get_book_data(bookLinks):
-	response = requests.get(bookLinks)
+#Pour récuperer toutes les informations d'un livre
+def get_book_data(url):
+	bookData = {}
+	response = requests.get(url)
 	if response.ok:
-		soup = BeautifulSoup(response.text, 'html.parser')
+		soup = BeautifulSoup(response.content, 'lxml')
 		trs = soup.findAll('tr')
 		bookData['url'] = url
 		bookData['upc'] = trs[0].find('td').text
@@ -56,22 +54,27 @@ def get_book_data(bookLinks):
 		bookData['image_url'] = pic.replace('../..', 'http://books.toscrape.com')
 	return bookData
 
-#Pour télécharger la première couverture du livre (comment creer les fichiers dans le dossier pictureData)
 try:
 	os.mkdir('pictureData')
+	os.mkdir('Data')
+except:
+	pass
 
-get_categories(url)
-for link in ctgLinks:
-	link.get_book_urls(ctglinks)
-	b = ctglinks.replace('http://books.toscrape.com/catalogue/category/books/','').replace('/index.html','')
-	os.mkdir(b)
-	file = open(b/'bookData.csv', 'w+')
-	file.write('product_page_url, universal_ product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url, \n')		
-	for book in bookLinks:
-		book.get_book_data(bookLinks)
-		for info in bookData:
-			file.write(str(bookLinks)+','+str(bookData['upc'])+','+bookData['title']+','+str(bookData['price_including_tax'])+','+str(bookData['price_excluding_tax'])+','+str(bookData['number_available'])+','+bookData['product_description'].replace('\n','')+','+bookData['category']+','+bookData['review_rating']+','+str(bookData['image_url']+"\n"))
-			file.close()	
+ctgLinks = get_categories(url)
+for url in ctgLinks:
+	b = url.replace('http://books.toscrape.com/catalogue/category/books/','')
+	file = open('Data/'+ b +'.csv', 'w+')
+	file.write('product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url,\n')		
+	bookLinks = get_book_urls(url)
+	for book_url in bookLinks:
+		bookData = get_book_data(book_url)
+		file.write(
+			book_url+','+str(bookData['upc'])+','+bookData['title'].replace(',','')+
+			','+str(bookData['price_including_tax'])+','+str(bookData['price_excluding_tax'])+
+			','+str(bookData['number_available'])+','+bookData['product_description'].replace('\n','').replace(',','')+
+			','+bookData['category']+','+bookData['review_rating']+','+str(bookData['image_url']+"\n")
+		)
+	file.close()	
 
 for photo in bookData['image_url']:
 	picture = requests.get(bookData['image_url'])
